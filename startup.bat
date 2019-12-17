a58426dbeb51873dfd5a62ed5eb5a36ef5237398
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-12-17 11:26:39 +0900

    インストールコマンドをstart upに追加

--- startup.bat
+++ startup.bat
@@ -1,3 +1,3 @@
 pip install bottle --proxy proxy.denpa.ac.jp:8080
-pip install MySQL-Python --proxy proxy.denpa.ac.jp:8080
+python -m pip install mysqlclient-1.4.6-cp38-cp38-win32.whl --proxy proxy.denpa.ac.jp:8080
 pause
