b52575e621dfd2ede2f6faba65734148232951ad
msm-s <ms1756@mailg.denpa.ac.jp>
2019-12-19 14:20:35 +0900

    add_limit

--- src/DBAccess.py
+++ src/DBAccess.py
@@ -1,18 +1,21 @@
 import MySQLdb
 
 class DBAccess:
+
+    sql_limit="LIMIT 1, 10"
+
     def __init__(self) :
         self.connection = MySQLdb.connect(
             host='localhost',
             user='root',
-            db='music',
+            db='music_site',
             # passeord='',
             charset='utf8'
         )
 
     def get_music_list(self) :
         cursor = self.connection.cursor()
-        cursor.execute("SELECT * FROM music")
+        cursor.execute("SELECT * FROM music"+ limit)
         itme_list = []
         for row in cursor:
             itme_list.append({
