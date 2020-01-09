41ace723dd25e4af68e5d302fdca0901d4a37199
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-01-09 10:40:13 +0900

    detailのjsonデータを変更

--- src/DBAccess.py
+++ src/DBAccess.py
@@ -17,7 +17,7 @@ class DBAccess:
         cursor = self.connection.cursor()
         cursor.execute(
             "SELECT id, song_name, artist_name, play_time, CASE WHEN bpm BETWEEN 0 AND 50 THEN 1 WHEN bpm BETWEEN 50 AND 100 THEN 2 WHEN bpm > 100 THEN 3 END AS bpm_division FROM music ORDER BY RAND() LIMIT 10")
-        itme_list = []
+        itmes_list = []
         for row in cursor:
             itme_list.append({
                 "id": row[0],
@@ -31,13 +31,12 @@ class DBAccess:
     def get_detail_music(self, music_id):
         cursor = self.connection.cursor()
         cursor.execute("SELECT id, song_name, artist_name, play_time, bpm FROM music WHERE id = %s", music_id)
-        itme_list = []
-        for row in cursor:
-            itme_list.append({
-                "id": row[0],
-                "song_name": row[1],
-                "artist_name": row[2],
-                "play_time": str(row[3]),
-                "bpm": row[4]
-            })
-        return {"data" : itme_list}
+        row = cursor.fetchone()
+        itme_list = {
+            "id": row[0],
+            "song_name": row[1],
+            "artist_name": row[2],
+            "play_time": str(row[3]),
+            "bpm": row[4]
+        }
+        return itme_list
