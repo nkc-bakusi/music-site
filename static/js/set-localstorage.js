7128344f280e1c4492277b026e68f34aee10bf26
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-01-13 20:46:19 +0900

    bpm_divisionをストレージに入れる

--- static/js/set-localstorage.js
+++ static/js/set-localstorage.js
@@ -1,3 +1,20 @@
+let search_data = JSON.parse(localStorage.getItem('search_data'));
+if(!search_data) {
+    search_data = {
+        "bpm_division": [
+            0,
+            0,
+            0
+        ],
+        "play_time_division": [
+            0,
+            0,
+            0
+        ]
+    };
+    localStorage.setItem('search_data', JSON.stringify(search_data));
+}
+
 /**
  * 指定された要素にクリックイベントを追加
  * @param target_dom
@@ -5,7 +22,7 @@
  */
 function add_storage_event(target_dom, target_music_data) {
     $(target_dom).click(function (event) {
-        console.log(event);
-        console.log(target_music_data);
+            search_data.bpm_division[target_music_data.bpm_division - 1]++;
+            localStorage.setItem('search_data', JSON.stringify(search_data));
     });
 }
