85d7b75cb01d7ff15cd4b1a2e26fb4f209bcf462
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-01-13 21:15:36 +0900

    indexのリストに詳細画面に対してのリンクを追加

--- static/js/index.js
+++ static/js/index.js
@@ -19,6 +19,7 @@ function getMusicData(){
             Object.keys(songdata.data).forEach(function(key){
                 var val = this[key];
                 var dom = $('.musicList .song').first().clone();
+                    dom.find('a').attr('href', '/detail/' + val.id);
                     dom.find('.songName').text(val.song_name);
                     dom.find('.songArtist').text(val.artist_name);
                     dom.find('.songLength').text(val.play_time);
