e62646982bc2bcb1bf1908fb2cbbaadb2cd6c4d7
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2020-01-17 10:13:47 +0900

    play_timeの区分をsqlに追加

--- src/DBAccess.py
+++ src/DBAccess.py
@@ -1,6 +1,7 @@
 import MySQLdb
 import json
 
+
 class DBAccess:
 
     sql_limit = 'LIMIT 1, 10'
@@ -17,7 +18,13 @@ class DBAccess:
     def get_music_list(self):
         cursor = self.connection.cursor()
         cursor.execute(
-            'SELECT id, song_name, artist_name, play_time, CASE WHEN bpm BETWEEN 0 AND 50 THEN 1 WHEN bpm BETWEEN 50 AND 100 THEN 2 WHEN bpm > 100 THEN 3 END AS bpm_division FROM music ORDER BY RAND() LIMIT 10')
+            'SELECT id, song_name, artist_name, play_time, '
+            'CASE WHEN bpm BETWEEN 0 AND 50 THEN 1 WHEN bpm '
+            'BETWEEN 50 AND 100 THEN 2 WHEN bpm > 100 THEN 3 END AS bpm_division, '
+            'CASE WHEN play_time BETWEEN \'00:00:00\' AND \'02:59:00\' THEN 1 WHEN play_time '
+            'BETWEEN \'03:00:00\' AND \'03:59:00\' THEN 2 WHEN play_time > \'04:00:00\' THEN 3 END AS play_time_division '
+            'FROM music ORDER BY RAND() LIMIT 10'
+        )
         itmes_list = []
         for row in cursor:
             itmes_list.append({
@@ -25,9 +32,10 @@ class DBAccess:
                 'song_name': row[1],
                 'artist_name': row[2],
                 'play_time': str(row[3]),
-                'bpm_division': row[4]
+                'bpm_division': row[4],
+                'play_time_division': row[5]
             })
-        return json.dumps({'data' : itmes_list})
+        return json.dumps({'data': itmes_list})
 
     def get_music_list_playtime(self, playtime):
         cursor = self.connection.cursor()
@@ -42,15 +50,15 @@ class DBAccess:
                 'play_time': str(row[3]),
                 'bpm_division': row[4]
             })
-        return json.dumps({'data' : itmes_list})
+        return json.dumps({'data': itmes_list})
 
     def get_music_list_bpm(self, bpm):
         sql = 'SELECT id, song_name, artist_name, play_time, CASE WHEN bpm BETWEEN 0 AND 50 THEN 1 WHEN bpm BETWEEN 50 AND 100 THEN 2 WHEN bpm > 100 THEN 3 END AS bpm_division, bpm FROM music '
-        if (bpm == '1') :
+        if (bpm == '1'):
             sql += 'WHERE bpm BETWEEN 0 and 49 '
-        elif (bpm == '2') :
+        elif (bpm == '2'):
             sql += 'WHERE bpm BETWEEN 50 and 100 '
-        else :
+        else:
             sql += 'WHERE bpm > 100 '
         sql += 'ORDER BY RAND() LIMIT 10'
         cursor = self.connection.cursor()
@@ -63,13 +71,14 @@ class DBAccess:
                 'artist_name': row[2],
                 'play_time': str(row[3]),
                 'bpm_division': row[4],
-                'bpm' : row[5]
+                'bpm': row[5]
             })
-        return json.dumps({'data' : itmes_list})
+        return json.dumps({'data': itmes_list})
 
     def get_detail_music(self, music_id):
         cursor = self.connection.cursor()
-        cursor.execute('SELECT id, song_name, artist_name, play_time, bpm FROM music WHERE id = %s', music_id)
+        cursor.execute(
+            'SELECT id, song_name, artist_name, play_time, bpm FROM music WHERE id = %s', music_id)
         row = cursor.fetchone()
         itme_list = {
             'id': row[0],
